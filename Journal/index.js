5d9a2069de0ed98cc8b356988f29f74750aaa037
RealChoki <rafaatchoki1999@gmail.com>
2023-04-10 17:42:51 +0200

    F

---
+++ Journal/index.js
@@ -0,0 +1,13 @@
+const view = document.getElementById("view");
+
+view.addEventListener("click", () => {
+  view.style.display = "none";
+});
+
+
+
+//TODO: i need to make a function to get the data from the database
+
+//! this function will get the data from the database
+
+//? what am i going to do here?
