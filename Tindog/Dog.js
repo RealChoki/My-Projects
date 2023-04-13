3dbfec2a440750a9146f20c0bf5b62f7b1085c28
RealChoki <rafaatchoki1999@gmail.com>
2023-04-13 15:05:34 +0200

    used data to rander

--- Tindog/Dog.js
+++ Tindog/Dog.js
@@ -1,1 +1,24 @@
-// Create the Dog class here
+import dogs from "./data.js";
+
+class dog {
+    constructor(data){
+    Object.assign(this, data)
+    }
+
+    getDogHtml(){
+        const {name, avatar, age, bio} = this
+        return `
+            <img class="big" src=${avatar}>
+            <img class="popup" src="images/badge-like.png">
+            <img class="popup" src="images/badge-nope.png">
+            <div class="text">
+                <h1>${name}</h1>
+                <h1>${age}</h1>
+                <h2>${bio}</h2>
+            </div>
+        `
+
+    }
+}
+
+export default dog
--- Tindog/index.js
+++ Tindog/index.js
@@ -1,1 +1,15 @@
 // Remember to import the data and Dog class!
+import dog from "./Dog.js"
+import dogs from "./data.js"
+
+function getNewDog(){
+    return dogs.shift()
+}
+
+function render() {
+    document.getElementById('feed').innerHTML = teddy.getDogHtml()
+}
+
+let teddy = new dog(getNewDog())
+
+render()
