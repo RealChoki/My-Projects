f29ddfbf84ec1cd181ab5ef1a55ddffbd8c6a35e
RealChoki <rafaatchoki1999@gmail.com>
2023-04-16 18:43:17 +0200

    fixed the rendering!!

--- Tindog/index.js
+++ Tindog/index.js
@@ -1,14 +1,48 @@
-// Remember to import the data and Dog class!
 import dog from "./Dog.js"
 import dogs from "./data.js"
 
+let isWaiting = false
+
 function getNewDog(){
-    return dogs.shift()
+    if(dogs.length > 0){
+        return dogs[0]
+    } 
+    
+}
+
+
+function actOnBtnPress(btnid , popupid){
+    document.getElementById(btnid).addEventListener('click', function(){
+        if (!isWaiting) {
+            isWaiting = true;
+            handleSwipe(popupid);
+            setTimeout(() => {
+                isWaiting = false;
+            }, 1000);
+        }
+    })
+}
+
+function handleSwipe(popupid){
+    document.getElementById(popupid).style.display = "block"
+    
+    if(dogs.length > 1){
+        setTimeout(()=> {
+            dogs.shift()
+            teddy = new dog(getNewDog())
+            render()
+        },1000)
+    } else if(dogs.length === 1) {
+        document.getElementById("footer").style.display = "none"
+    }
 }
 
 function render() {
     document.getElementById('feed').innerHTML = teddy.getDogHtml()
 }
+
+actOnBtnPress("like", "likepop")
+actOnBtnPress("dislike", "dislikepop")
 
 let teddy = new dog(getNewDog())
 
