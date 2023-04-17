253061f4f5b3d862d133526e957744e09b73a503
RealChoki <rafaatchoki1999@gmail.com>
2023-04-17 22:24:59 +0200

    Finally fixed grid in mediaquery

--- Journal/index.js
+++ Journal/index.js
@@ -4,26 +4,32 @@ view.addEventListener("click", () =>{
   let feedHtml = ''
 
   feedHtml = `
-    <div class="content grid">
-        <img src="images/blog-image-05.png">
-        <h5>JULY 23, 2022</h5>
-        <h1>Blog three</h1>
-        <p>I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>   
-    </div> 
+    <section>
+      <div class="content">
+          <img src="images/article-image-02.png">
+          <h5>JULY 23, 2022</h5>
+          <h1>Blog three</h1>
+          <p>I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>   
+      </div> 
+    </section>
 
-    <div class="content grid">
-        <img src="images/article-image-03.png">
-        <h5>JULY 23, 2022</h5>
-        <h1>Blog two</h1>
-        <p>I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>  
-    </div> 
-  
-    <div id="last-blog" class="content grid">
-        <img src="images/article-image-02.png">
-        <h5>JULY 23, 2022</h5>
-        <h1>Blog one</h1>
-        <p>I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
-    </div>
+    <section>
+      <div class="content">
+          <img src="images/article-image-03.png">
+          <h5>JULY 23, 2022</h5>
+          <h1>Blog two</h1>
+          <p>I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>  
+      </div> 
+    </section>
+
+    <section>
+      <div id="last-blog" class="content">
+          <img src="images/article-image-02.png">
+          <h5>JULY 23, 2022</h5>
+          <h1>Blog one</h1>
+          <p>I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
+      </div>
+    </section>
   `
   view.style.display = "none"
   document.getElementById("last-blog").style.marginBottom = "40px"
