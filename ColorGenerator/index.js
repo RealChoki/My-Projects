8be1de5469df9a7c841b0c15e9bc29fe1358cd99
RealChoki <rafaatchoki1999@gmail.com>
2023-04-30 13:18:59 +0200

    changed naming

--- ColorGenerator/index.js
+++ ColorGenerator/index.js
@@ -2,31 +2,28 @@ fetch("https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,3
     .then((res) => res.json())
     .then((data) => {
         document.getElementById("btn").addEventListener("click", () => {
-           
-           console.log(data)
-           const hexValue = data.colors
-           console.log(hexValue.hex)
+            const apiValue = data.colors
            document.getElementById("feed").innerHTML = `
            
             <div class="color-field">
-                <img class="color" src=${hexValue[0].image.bare}>
-                <p class="color-code">${hexValue[0].hex.value}</p>
+                <img class="color" src=${apiValue[0].image.bare}>
+                <p class="color-code">${apiValue[0].hex.value}</p>
             </div>
             <div class="color-field">
-                <img class="color" src=${hexValue[1].image.bare}>
-                <p class="color-code">${hexValue[1].hex.value}</p>
+                <img class="color" src=${apiValue[1].image.bare}>
+                <p class="color-code">${apiValue[1].hex.value}</p>
             </div>
             <div class="color-field">
-                <img class="color" src=${hexValue[2].image.bare}>
-                <p class="color-code">${hexValue[2].hex.value}</p>
+                <img class="color" src=${apiValue[2].image.bare}>
+                <p class="color-code">${apiValue[2].hex.value}</p>
             </div>
             <div class="color-field">
-                <img class="color" src=${hexValue[3].image.bare}>
-                <p class="color-code">${hexValue[3].hex.value}</p>
+                <img class="color" src=${apiValue[3].image.bare}>
+                <p class="color-code">${apiValue[3].hex.value}</p>
             </div>
             <div class="color-field">
-                <img class="color" src=${hexValue[4].image.bare}>
-                <p class="color-code">${hexValue[4].hex.value}</p>
+                <img class="color" src=${apiValue[4].image.bare}>
+                <p class="color-code">${apiValue[4].hex.value}</p>
             </div>
            `
         })
