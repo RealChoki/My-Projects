1f143b9b8eefefea2648121bc510e06b9809bfc6
RealChoki <rafaatchoki1999@gmail.com>
2023-04-30 20:13:17 +0200

    Style change border of colorpicker/render on load

--- ColorGenerator/index.js
+++ ColorGenerator/index.js
@@ -1,4 +1,5 @@
 document.getElementById("btn").addEventListener("click", ()=>{
+    const feed = document.getElementById("feed")
     const color = document.getElementById("selected-color").value.slice(1)
     const schemes = document.getElementById("color-schemes").value.toLowerCase()
     const actualData = `https://www.thecolorapi.com/scheme?hex=${color}&mode=${schemes}&count=5`
@@ -6,16 +7,20 @@ document.getElementById("btn").addEventListener("click", ()=>{
         .then((res) => res.json())
         .then((data) => {
             const apiValue = data.colors
+            feed.innerHTML = ""
             for (let i = 0; i < 5; i++){
-                    document.getElementById("feed").innerHTML += `
-                    <div class="color-field">
+                    feed.innerHTML += `
+                    <div>
                         <img class="color" src=${apiValue[i].image.bare}>
                         <p class="color-code">${apiValue[i].hex.value}</p>
                     </div>
                 `
             }
+
     })
 })
+
+document.getElementById("btn").click()
 
    
 
