d7e60b34d50449fce1a3c87ac27cacbbdb8c60f6
RealChoki <rafaatchoki1999@gmail.com>
2023-05-14 21:17:16 +0200

    Working fetch request. can display (Title/Poster)

--- API/co0a24985872b20458f37bc22/index.js
+++ API/co0a24985872b20458f37bc22/index.js
@@ -1,5 +1,12 @@
 document.getElementById("search-btn").addEventListener("click", ()=>{
-    fetch("http://www.omdbapi.com/?apikey=a03264ab")
+    document.querySelector(".placeholder-search").classList.add("hide")
+    fetch(`http://www.omdbapi.com/?apikey=a80cae51&s=${document.getElementById("search-field").value}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        document.getElementById("feed").innerHTML = `
+            <img src="${data.Search[0].Poster}">   
+            <h3>${data.Search[0].Title}</h3>
+        `
+    })
 })
