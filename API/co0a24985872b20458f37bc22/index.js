2b97245d03ca2119d39cf102f00511d0845ee8e8
RealChoki <rafaatchoki1999@gmail.com>
2023-05-15 20:50:42 +0200

    Added fetch for imdbID and did styling

--- API/co0a24985872b20458f37bc22/index.js
+++ API/co0a24985872b20458f37bc22/index.js
@@ -1,12 +1,34 @@
 document.getElementById("search-btn").addEventListener("click", ()=>{
     document.querySelector(".placeholder-search").classList.add("hide")
     fetch(`http://www.omdbapi.com/?apikey=a80cae51&s=${document.getElementById("search-field").value}`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        document.getElementById("feed").innerHTML = `
-            <img src="${data.Search[0].Poster}">   
-            <h3>${data.Search[0].Title}</h3>
-        `
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            fetch(`http://www.omdbapi.com/?apikey=a80cae51&i=${data.Search[0].imdbID}`)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const{Poster, Title, Plot, Genre, Runtime,imdbRating} = data
+                    document.getElementById("feed").innerHTML = `
+                        <img src="${Poster}">   
+                        <div>
+                            <div class="first row">
+                                <h3>${Title}</h3>
+                                <img src="images/star.png">
+                                <h6>${imdbRating}</h6>
+                            </div>
+                            <div class="secound row">
+                                <h5>${Runtime}</h5>
+                                <h5>${Genre}</h5>
+                                <div class="watchlist">
+                                    <img src="images/plus.png">
+                                    <h5>Watchlist</h5>
+                                </div>
+                            </div>
+                            <p class="plot">${Plot}</p>
+                        </div>
+                    
+            `
+        }) 
     })
 })
