581e2c4e8ade4116bbad61100665d2d1a594b3be
RealChoki <rafaatchoki1999@gmail.com>
2023-06-16 00:14:27 +0200

    finished Tournament (added main / getWinner)

--- Java/Neuer Ordner/Tournament.java
+++ Java/Neuer Ordner/Tournament.java
@@ -1,75 +1,138 @@
 import java.util.Arrays;
 
+/**
+ * Represents a tournament with multiple teams.
+ */
 public class Tournament {
     private int gameCount;
     private Team[] teams;
 
-    public Tournament(int teamsAmount){
-        if(teamsAmount < 2){
+    /**
+     * Creates a new tournament with the specified number of teams.
+     * If the number of teams is less than 2, it defaults to 2.
+     *
+     * @param teamsAmount the number of teams in the tournament
+     */
+    public Tournament(int teamsAmount) {
+        if (teamsAmount < 2) {
             teamsAmount = 2;
         }
         teams = new Team[teamsAmount];
+        gameCount = 0;
     }
 
-    public Team[] getTeams(){
-        return this.teams;
+    /**
+     * Returns the array of teams in the tournament.
+     *
+     * @return the teams in the tournament
+     */
+    public Team[] getTeams() {
+        return teams;
     }
 
-    public int getGameCount(){
-        return this.gameCount;
+    /**
+     * Returns the number of games played in the tournament.
+     *
+     * @return the game count
+     */
+    public int getGameCount() {
+        return gameCount;
     }
 
-    public void registerTeam(Team team){
-        for (int i=0 ; i< teams.length; i++){
-            if(teams[i] == null){
+    /**
+     * Registers a team in the tournament.
+     * If all spots are filled, the team is not registered.
+     *
+     * @param team the team to register
+     */
+    public void registerTeam(Team team) {
+        for (int i = 0; i < teams.length; i++) {
+            if (teams[i] == null) {
                 teams[i] = team;
+                break;
             }
         }
     }
 
-    public boolean isReady(){
-        for (Team spots: teams){
-            if(spots != null){
-                return true;
+    /**
+     * Checks if the tournament is ready to start.
+     * Returns true if all spots in the tournament are filled with teams, false otherwise.
+     *
+     * @return true if the tournament is ready, false otherwise
+     */
+    public boolean isReady() {
+        for (Team spot : teams) {
+            if (spot == null) {
+                return false;
             }
         }
-
-        return false;
+        return true;
     }
 
-    public void playGame(Team team1, Team team2){
+    /**
+     * Plays a game between two teams in the tournament.
+     * The teams compete in at least three rounds, and the winning team gets points based on the random scores generated.
+     * The game count is incremented after each game played.
+     *
+     * @param team1 the first team
+     * @param team2 the second team
+     */
+    public void playGame(Team team1, Team team2) {
         System.out.println(team1.getTeamName() + " vs " + team2.getTeamName());
-        for (int i=0 ; i<3; i++){
-            int firstRandomScore = (int) Math.random() * 10;
-            int secondRandomScore = (int) Math.random() * 10;
-          
-            if(firstRandomScore > secondRandomScore){
+        int rounds = 3;
+        for (int i = 0; i < rounds; i++) {
+            int firstRandomScore = (int) (Math.random() * 10);
+            int secondRandomScore = (int) (Math.random() * 10);
+
+            if (firstRandomScore > secondRandomScore) {
                 team1.addScore(firstRandomScore);
-            }else if (secondRandomScore > firstRandomScore){
+            } else if (secondRandomScore > firstRandomScore) {
                 team2.addScore(secondRandomScore);
-            }else{
+            } else {
                 team1.addScore(firstRandomScore);
                 team2.addScore(secondRandomScore);
             }
-
-            this.gameCount++;
         }
+        this.gameCount++;
     }
 
-    public void playChampionship(){
-        for (int i = 0; i < teams.length; i++){
-            for (int j = teams.length; j > 0; j--){
-                playGame(teams[i], teams[j]);
-
-                if(teams[i].getScore() > teams[j].getScore()){
-                    System.out.println(teams[i].getTeamName() + " won this round !!");
-                }else if(teams[j].getScore() > teams[i].getScore()){
-                    System.out.println(teams[j].getTeamName() + " won this round !!");
+    /**
+    * Plays the championship round of the tournament.
+    * All teams in the tournament play against each other at least twice.
+    * The winners of each game are displayed, and the final scores and overall winners are printed.
+    */
+    public void playChampionship() {
+        for (int round = 0; round < 2; round++) {
+            System.out.println("Round " + (round + 1) + ":");
+            for (int i = 0; i < teams.length; i++) {
+                for (int j = i + 1; j < teams.length; j++) {
+                    playGame(teams[i], teams[j]);
+    
+                    if (teams[i].getScore() > teams[j].getScore()) {
+                        System.out.println("The " + teams[i].getTeamName() + " won this round !!");
+                    } else if (teams[j].getScore() > teams[i].getScore()) {
+                        System.out.println("The " + teams[j].getTeamName() + " won this round !!");
+                    } else {
+                        System.out.println("It's a tie between " + teams[i].getTeamName() + " and " + teams[j].getTeamName());
+                    }
                 }
             }
         }
+        System.out.println("Final scores:");
+        printScore();
+        System.out.println("The Overall Winner(s):");
+        Team[] winners = getWinner();
+        for (Team team : winners) {
+            System.out.println(team.getTeamName());
+        }
     }
-
+    
+    /**
+    Prints the scores of the teams in the tournament.
+    If no games have been played yet, it prints a message indicating that no games have taken place.
+    Otherwise, it sorts the teams based on their scores in descending order using the bubble sort algorithm.
+    It then prints the team names and their corresponding scores.
+    */
     public void printScore() {
     if (gameCount == 0) {
         System.out.println("Bisher fand kein Spiel statt.");
@@ -95,91 +158,67 @@ public class Tournament {
     }
 }
 
-//highscore, look how many have same highscore,  Winnercount goes up, inisilize list with winner count, put winners in list
+    /**
+    Determines the winner(s) of the tournament based on the scores.
+    If no games have been played yet, it returns an empty array of teams.
+    Otherwise, it finds the highest score and counts the number of teams with that score.
+    It then creates an array of the winner(s) and returns it.
+    @return an array of teams representing the winner(s) of the tournament
+    */
     public Team[] getWinner() {
-    if (getGameCount() == 0) {
-        return new Team[0];
-    } else {
-        int highScore = 0;
-        int winnerCount = 0;
+        if (getGameCount() == 0) {
+            return new Team[0];
+        } else {
+            int highScore = 0;
+            int winnerCount = 0;
             for (int i = 0; i < teams.length; i++) {
                 int score = teams[i].getScore();
 
-                if (score >= teams[i+1].getScore()) {
-                    highScore = score;  
-                    
-                }else{
-                    highScore = teams[i+1].getScore();
+                if (score >= highScore) {
+                    highScore = score;
                 }
             }
 
             for (int i = 0; i < teams.length; i++) {
-                if(teams[i].getScore() == highScore){
+                if (teams[i].getScore() >= highScore) {
                     winnerCount++;
                 }
             }
+            
             Team[] winner = new Team[winnerCount];
             int index = 0;
             
-            for (int i = 0; i < teams.length; i++){
-                if (teams[i].getScore() == highScore){
+            for (int i = 0; i < teams.length; i++) {
+                if (teams[i].getScore() >= highScore) {
                     winner[index] = teams[i];
                     index++;
                 }
-            }    
+            }
+            
             return winner;
         }
-    
+    }
+    /**
+    The main method to start the tournament.
+    Creates a new tournament with a specified number of teams and registers them.
+    If the tournament is ready, the championship is played and the results are displayed.
+    Finally, the scores and winners of the tournament are printed.
+    */
+    public static void main(String[] args){
+        Tournament tournament = new Tournament(4);
+
+        Team team1 = new Team("Straw Hats");
+        Team team2 = new Team("Blackbeards");
+        Team team3 = new Team("Marines");
+        Team team4 = new Team("Yonkos");
+
+        tournament.registerTeam(team1);
+        tournament.registerTeam(team2);
+        tournament.registerTeam(team3);
+        tournament.registerTeam(team4);
+
+        if (tournament.isReady()) {
+            tournament.playChampionship();
+        }
     }
 }
-/*
-
-
-
-
-. Der Gewinner des jeweiligen Spieles und der oder die Gewinner des gesamten Turnier sollen auf der Konsole ausgegeben werden.
-Es soll eine Methode printScore() entwickelt werden, welche die Namen aller Teams und deren Punktzahl absteigend sortiert nach deren Punktzahl auf der Konsole ausgibt. Solange noch kein Spiel stattgefunden hat, soll lediglich die Meldung 'Bisher fand kein Spiel statt.' auf der Konsole ausgegeben werden.
-Es soll eine Methode getWinner() entwickelt werden, welche den oder die Gewinner des Turnier nach Punkten (als Array vom Typ Team) zurückliefert. Solange noch kein Spiel stattgefunden hat, soll lediglich ein Array vom Typ Team mit der Größe 0 zurückgeliefert werden.
-Erstelle eine Main-Methode, die ein neues Turnier startet und mindestens vier Teams im Turnier registriert. Sobald alle Team bereit sind, soll (mindestens) eine Meisterschaft gespielt werden. Danach soll der Punktestand aller Teams auf der Konsole ausgeben werden und final der oder die Gewinner des Turniers angezeigt werden.
-
-PS: Es liegt an dir, ob du ein Quidditch-Turnier stattfinden lässt oder die Zauber:innen mit Hydro, Elektro-, Kryo- oder Pyrozauber gegeneinander antreten lässt oder auch nur ein einfaches Würfelturnier ausrichtest. Sei kreativ!
-
-
-
-
-
-
-
-
-
-
-
-
-
-Bitte erstelle eine Klasse Team
-- mit einem Attribut teamName vom Typ String und
-- mit einem Attribut score vom Typ int und
-- mit einer statischen Variable teamCounter vom Typ int
-und folgenden Funktionalitäten:
-
-Der Name des Teams darf nur über den Konstruktor gesetzt werden und niemals verändert werden.
-Der Zähler für alle jemals erstellten Teams (teamCounter) soll im Konstruktor im 1 erhöht werden.
-Für den Teamnamen und die Punkte des Teams soll jeweils eine Getter-Methode geschrieben werden.
-Für die Anzahl aller jemals erstellten Teams (teamCounter) soll eine statische Klassenmethode namens getTeamCounter() geschrieben werden.
-Es soll eine Methode addScore(int score) erstellt werden, welche die übergebene Punktzahl zur aktuellen Punktzahl des Teams addiert.
-Für die Klasse Team soll die toString-Methode implementiert werden.
-Darüber hinaus erstelle bitte eine Klasse Tournament
-- mit einem Array teams vom Typ Team
-- mit einem Attribut gameCount vom Typ int
-und folgenden Funktionalitäten:
-
-Die Anzahl der Teams im Turnier wird im Konstruktor übergeben. Wenn die Zahl kleiner als 2 ist, dann soll 2 angenommen werden, damit immer mindestens zwei Teams gegeneinander antreten.
-Sowohl für die Teams im Turnier (teams) als auch die Anzahl der gespielten Spiele (gameCount) soll eine Getter-Methode implementiert werden.
-Es soll eine Methode registerTeam(Team team) erstellt werden, welche ein neues Team im Spiel registriert. Wenn bereits alle freien Plätze im Turnier belegt sind, so tut die Methode nichts.
-Es soll eine Methode isReady() implementiert werden, welche true zurückgibt, wenn alle freien Plätze im Turnier belegt sind. Solange noch Plätze frei sind, soll immer false zurückgegeben werden.
-Es soll eine Methode playGame(Team team1, Team team2) implementiert werden, die zwei Teams gegeneinander antreten lässt. Zu Beginn der Methode sollen die Namen der Teams ausgegeben werden, die gegeneinander antreten. 
-Es soll dabei mindestens drei Spielrunden geben, in denen die Teams eine zufällige Anzahl von Punkten gewinnen. Final soll der Gewinner des Spiels die Punkte auf das Teamkonto gutgeschrieben bekommen. 
-Der Verlierer bekommt keine Punkte. Sollte das Spiel unentschieden ausgehen, so bekommen beide Teams die selbe Punktzahl. Mit jedem gespielten Spiel soll der Zähler für die gespielten Spiele (gameCount) um eins erhöht werden.
-Es soll eine Methode playChampionship() implementiert werden, die alle Teams eines Turniers mindestens zweimal gegeneinander antreten lässt
-
- */
